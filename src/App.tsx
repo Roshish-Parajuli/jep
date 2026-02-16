56a9d6393ffc8e2c846601b5efab71a9b93c6a68
Roshish Parajuli <roshishparajuli7@gmail.com>
2026-02-16 21:45:05 +0545

    fix: switch to HashRouter to fix production 404s

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 
 import LoginPage from './pages/LoginPage';
